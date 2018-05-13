54482e6cb78cb688cb24a7383dfd4b47d4d94fc3
Levente Takács <levi.takacs@gmail.com>
2018-05-14 00:38:32 +0200

    routing 90% done.

--- Traffic simulation/Assets/Scripts/CrossRoadMeta.cs
+++ Traffic simulation/Assets/Scripts/CrossRoadMeta.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrossRoadMeta : MonoBehaviour {
+public class CrossRoadMeta : MonoBehaviour
+{
 
   [System.Serializable]
   public class RoadGroup
@@ -12,13 +13,238 @@ public class CrossRoadMeta : MonoBehaviour {
   }
   public RoadGroup[] roadGroup;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  public List<GameObject> Children;
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public GameObject[] AddTheseToRoute(GameObject from, GameObject endObject, Vector3 forwardVector)
+  {
+    //1. ki kell találni mi ez a kezdőpont
+    //2. a lehetséges kimenetelekből ki kell választani a LEGKÖZELEBBIT az endObject-hez képest
+    //3. ha megvan a kijárat ki kell számolni, hogy az bal, jobb, vagy egyenes, és kiszámítani
+    List<GameObject> options = GetOptions(from);
+    Debug.Log("megvannak: " + options.Count);
+
+    GameObject theExit = ClosestExitPoint(options, endObject);
+
+    foreach (Transform child in transform)
+    {
+      if (child.tag == "CrossRoad")
+      {
+        Children.Add(child.gameObject);
+      }
+    }
+    Debug.Log("exit point: " + theExit);
+
+    GameObject[] theRouteElements = GetDirectionObjects(from, theExit, forwardVector);
+
+    return theRouteElements;
+  }
+
+  List<GameObject> GetOptions(GameObject from)
+  {
+    foreach (var item in roadGroup)
+    {
+      if (item.from.Equals(from))
+      {
+        return item.options;
+      }
+    }
+
+    return null;
+  }
+
+  GameObject ClosestExitPoint(List<GameObject> options, GameObject endObject)
+  {
+    if (options != null)
+    {
+      float dist = -1F;
+      float distnew;
+      GameObject tobe = null;
+      for (int i = 0; i < options.Count; i++)
+      {
+        if (tobe == null)
+        {
+          dist = Vector3.Distance(endObject.GetComponent<TransformModifier>().Position, options[i].GetComponent<TransformModifier>().Position);
+          tobe = options[i];
+          Debug.Log("init....  " + tobe.name + "    ..  " + dist);
+          continue;
+        }
+
+        distnew = Vector3.Distance(endObject.GetComponent<TransformModifier>().Position, options[i].GetComponent<TransformModifier>().Position);
+        
+        if (distnew < dist)
+        {
+          dist = distnew;
+          tobe = options[i];
+        }
+        Debug.Log("....  " + options[i].name + "    ..  " + distnew + ".... " + dist);
+
+      }
+
+      return tobe;
+    }
+
+    return null;
+
+  }
+
+  GameObject[] GetDirectionObjects(GameObject from, GameObject exit, Vector3 forwardVector)
+  {
+    Debug.Log("forward: " + forwardVector);
+    float angleRight = Vector3.SignedAngle(forwardVector, (GameObject.Find("SM_Env_Road_Crossing_11").GetComponent<TransformModifier>().Position - from.GetComponent<TransformModifier>().Position).normalized, Vector3.up);
+    float angleForward = Vector3.SignedAngle(forwardVector, (GameObject.Find("SM_Env_Road_Crossing_13").GetComponent<TransformModifier>().Position - from.GetComponent<TransformModifier>().Position).normalized, Vector3.up);
+    float angleLeft = Vector3.SignedAngle(forwardVector, (GameObject.Find("SM_Env_Road_Crossing_32").GetComponent<TransformModifier>().Position - from.GetComponent<TransformModifier>().Position).normalized, Vector3.up);
+
+    float angleExit = Vector3.SignedAngle(forwardVector, (exit.GetComponent<TransformModifier>().Position - from.GetComponent<TransformModifier>().Position).normalized, Vector3.up);
+    Debug.Log("kanyarodási szög: " + angleExit);
+    GameObject[] theArray;
+    if (angleExit > 40f)
+    {
+      theArray = RightPath(from, forwardVector);
+    }
+    else if(angleExit < -40f)
+    {
+      theArray = LeftPath(from, forwardVector);
+    }
+    else
+    {
+      theArray = ForwardPath(from, forwardVector);
+    }
+
+    foreach (var item in theArray)
+    {
+      Debug.Log(item.name);
+    }
+    return theArray;
+  }
+
+  private GameObject[] RightPath(GameObject roadItem, Vector3 direction)
+  {
+    Vector3 rotatedVectorBy90 = Quaternion.Euler(0, 90, 0) * direction;
+    List<GameObject> rightPath = new List<GameObject>();
+    Vector3 newInitialPoint = roadItem.GetComponent<TransformModifier>().Position;
+    Vector3 forward = direction * 5;
+
+    rightPath.Add(roadItem);
+
+    for (int i = 0; i < 2; i++)
+    {
+      foreach (var item in Children)
+      {
+        GameObject temp = GetClosestInsideCrossElement(newInitialPoint + forward);
+
+        if (!temp.Equals(rightPath[rightPath.Count - 1]))
+        {
+          newInitialPoint = temp.GetComponent<TransformModifier>().Position;
+          if (i == 0)
+          {
+            forward = rotatedVectorBy90*5;
+            rightPath.Add(temp);
+            break;
+          }
+          else
+          {
+            rightPath.Add(temp);
+          }
+
+          break;
+        }
+      }
+    }
+
+    return rightPath.ToArray();
+  }
+  private GameObject[] LeftPath(GameObject roadItem, Vector3 direction)
+  {
+    Vector3 rotatedVectorBy90 = Quaternion.Euler(0, -90, 0) * direction;
+    List<GameObject> leftPath = new List<GameObject>();
+    Vector3 newInitialPoint = roadItem.GetComponent<TransformModifier>().Position;
+    Vector3 forward = direction * 5;
+
+    leftPath.Add(roadItem);
+
+    for (int i = 0; i < 4; i++)
+    {
+      foreach (var item in Children)
+      {
+        GameObject temp = GetClosestInsideCrossElement(newInitialPoint + forward);
+
+        if (!temp.Equals(leftPath[leftPath.Count - 1]))
+        {
+          newInitialPoint = temp.GetComponent<TransformModifier>().Position;
+          if (i == 1)
+          {
+            forward = rotatedVectorBy90 * 5;
+            leftPath.Add(temp);
+            break;
+          }
+          else
+          {
+            leftPath.Add(temp);
+          }
+
+          break;
+        }
+      }
+    }
+
+    return leftPath.ToArray();
+  }
+
+  private GameObject[] ForwardPath(GameObject roadItem, Vector3 direction)
+  {
+    List<GameObject> forwardPath = new List<GameObject>();
+    Vector3 newInitialPoint = roadItem.GetComponent<TransformModifier>().Position;
+    Vector3 forward = direction*5;
+
+    forwardPath.Add(roadItem);
+
+    for (int i = 0; i < Children.Count; i++)
+    {
+      foreach (var item in Children)
+      {
+        GameObject temp = GetClosestInsideCrossElement(newInitialPoint + forward);
+
+        if (!temp.Equals(forwardPath[forwardPath.Count - 1]))
+        {
+          forwardPath.Add(temp);
+          newInitialPoint = temp.GetComponent<TransformModifier>().Position;
+        }
+      }
+    }
+
+    return forwardPath.ToArray();
+  }
+
+  public GameObject GetClosestInsideCrossElement(Vector3 possiblePos)
+  {
+    GameObject theChoosenOne = null;
+    foreach (var item in Children)
+    {
+      if (theChoosenOne == null)
+      {
+        theChoosenOne = item;
+      }
+      float dist = Vector3.Distance(possiblePos, theChoosenOne.GetComponent<TransformModifier>().Position);
+      float newDist = Vector3.Distance(possiblePos, item.GetComponent<TransformModifier>().Position);
+
+      if (newDist < dist)
+      {
+        theChoosenOne = item;
+      }
+
+    }
+    return theChoosenOne;
+  }
 }
--- Traffic simulation/Assets/Scripts/PointHandlerScript.cs
+++ Traffic simulation/Assets/Scripts/PointHandlerScript.cs
@@ -7,19 +7,37 @@ public class PointHandlerScript : MonoBehaviour
 {
   GameObject[] allRoadGameObject;
   GameObject[] allCrossRoadGameObject;
+  List<GameObject> allRoad;
 
   public GameObject car;
   public GameObject popcorn;
   public GameObject closestObject;
-  public Vector3 forwardVector;
   public GameObject endObject;
+  public Vector3 forwardVector;
+
+  public List<GameObject> route;
 
   // Use this for initialization
   void Awake()
   {
-    //PathPointRecreateAndNull();
+    allRoadGameObject = GameObject.FindGameObjectsWithTag("RoadElement");
+    allCrossRoadGameObject = GameObject.FindGameObjectsWithTag("CrossRoad");
+    allRoad = new List<GameObject>();
+    allRoad.AddRange(allRoadGameObject);
+    allRoad.AddRange(allCrossRoadGameObject);
+
+    route = new List<GameObject>();
+
+    PathPointRecreateAndNull();
 
     PathCalculator();
+
+    if(car !=null && popcorn != null && closestObject != null && endObject != null && forwardVector != null)
+    {
+      route.Add(closestObject);
+      RouteCalculator();
+    }
+    
   }
 
   // Update is called once per frame
@@ -30,10 +48,10 @@ public class PointHandlerScript : MonoBehaviour
 
   public void PathPointRecreateAndNull()
   {
-    var foundObjects = FindObjectsOfType<TransformModifier>();
+    var foundObjects = FindObjectsOfType<CrossRoadModel>();
     foreach (var item in foundObjects)
     {
-      var components = item.GetComponents(typeof(TransformModifier));
+      var components = item.GetComponents(typeof(CrossRoadModel));
       foreach (var comp in components)
       {
         //DestroyImmediate instead of Destroy!!!!
@@ -41,13 +59,23 @@ public class PointHandlerScript : MonoBehaviour
       }
     }
 
-    allRoadGameObject = GameObject.FindGameObjectsWithTag("RoadElement");
-    allCrossRoadGameObject = GameObject.FindGameObjectsWithTag("CrossRoad");
-
     foreach (var item in allRoadGameObject)
     {
+      var component = item.GetComponent<RoadElementModel>();
+      Vector3 nullvec = new Vector3(0f, 0f, 0f);
+      GameObject next = component.NextElement;
+
+      DestroyImmediate(component);
       item.AddComponent<RoadElementModel>().thisColor = Color.blue;
+
+      if (next != null)
+      {
+        item.GetComponent<RoadElementModel>().NextElement = next;
+
+      }
+      
     }
+
     foreach (var item in allCrossRoadGameObject)
     {
       item.AddComponent<CrossRoadModel>().thisColor = Color.cyan;
@@ -61,23 +89,24 @@ public class PointHandlerScript : MonoBehaviour
     forwardVector = car.transform.forward;
     closestObject = GetClosestElement(car.transform.position);
     endObject = GetClosestElement(popcorn.transform.position);
-
-    Debug.Log("mustang start object neve: " + closestObject.name + "  pozi: " + closestObject.GetComponent<RoadElementModel>().Position);
-    Debug.Log("dist: " + Vector3.Distance(car.transform.position, closestObject.GetComponent<RoadElementModel>().Position));
+    
+    Debug.Log("mustang start object neve: " + closestObject.name + "  pozi: " + closestObject.GetComponent<RoadElementModel>().Position +
+      "  dist: " + Vector3.Distance(car.transform.position, closestObject.GetComponent<RoadElementModel>().Position) +
+      "  forward: " + forwardVector);
     Debug.Log("popcorn end object neve: " + endObject.name);
   }
 
   public GameObject GetClosestElement(Vector3 carPos)
   {
     GameObject theChoosenOne = null;
-    foreach (var item in allRoadGameObject)
+    foreach (var item in allRoad)
     {
       if (theChoosenOne == null)
       {
         theChoosenOne = item;
       }
-      float dist = Vector3.Distance(carPos, theChoosenOne.GetComponent<RoadElementModel>().Position);
-      float newDist = Vector3.Distance(carPos, item.GetComponent<RoadElementModel>().Position);
+      float dist = Vector3.Distance(carPos, theChoosenOne.GetComponent<TransformModifier>().Position);
+      float newDist = Vector3.Distance(carPos, item.GetComponent<TransformModifier>().Position);
 
       if (newDist < dist)
       {
@@ -86,6 +115,90 @@ public class PointHandlerScript : MonoBehaviour
 
     }
     return theChoosenOne;
+  }
+
+  public void RouteCalculator()
+  {
+    
+    GameObject nextOne = nextElement();
+    while (nextOne.name != endObject.name)
+    {
+      route.Add(nextOne);
+      if(nextOne.tag == "RoadElement")
+      {
+        nextOne = nextElement();
+      }
+      if(nextOne.tag == "CrossRoad")
+      {
+        CrossRoadMeta parentMeta = nextOne.GetComponentInParent<CrossRoadMeta>();
+
+        //amit a függvény visszaad ahhoz még hozzá kell csapni a kereszteződés utáni első roadelementet!!!
+        GameObject[] addables = parentMeta.AddTheseToRoute(nextOne, endObject, 
+          (nextOne.GetComponent<TransformModifier>().Position 
+                - route[route.Count-1].GetComponent<TransformModifier>().Position).normalized);
+
+        route.AddRange(addables);
+
+        /*foreach (var item in route)
+        {
+          Debug.Log("route elements: " + item.name);
+        }
+        */
+
+        Vector3 newForward = (route[route.Count - 1].GetComponent<TransformModifier>().Position
+                - route[route.Count - 2].GetComponent<TransformModifier>().Position).normalized;
+
+        GameObject theNext = GetClosestElement(route[route.Count - 1].GetComponent<TransformModifier>().Position + newForward * 5);
+        Debug.Log("kereszteződés utáni elem: "+theNext.name+" ......   "+newForward);
+
+        nextOne = theNext;
+
+      }
+
+      if (route.Count > 50)
+      {
+        Debug.Log("route szám miatt ugrottunk ki");
+        return;
+      }
+      
+    }
+    Debug.Log("végzett a ciklus");
+    //ha a nextElement roadelement akkor megyünk tovább, ha crossroad akkor külön kezeljük
+  }
+
+  public GameObject nextElement()
+  {
+    GameObject theLast = route[route.Count - 1];
+    RoadElementModel theLastScript = theLast.GetComponent<RoadElementModel>();
+
+    if(theLastScript.NextElement != null)
+    {
+      Debug.Log(theLastScript.NextElement.name);
+      return theLastScript.NextElement;
+    }
+
+    if (route.Count == 1)
+    {
+      //1 elem van, a forward-ot használjuk
+
+      GameObject theNext = GetClosestElement(theLastScript.Position + forwardVector * 5);
+      Debug.Log(theNext.name);
+      return theNext;
+    }
+    else
+    {
+      GameObject beforeLast = route[route.Count - 2];
+      TransformModifier beforeLastScript = beforeLast.GetComponent<TransformModifier>();
+      Debug.Log(theLastScript.Position + "   ...   " + beforeLastScript.Position);
+      Vector3 tempForward = (theLastScript.Position - beforeLastScript.Position).normalized;
+
+      GameObject theNext = GetClosestElement(theLastScript.Position + tempForward * 5);
+      Debug.Log(theNext.name + "   ...   " + theNext.GetComponent<TransformModifier>().Position);
+
+      return theNext;
+
+    }
+    
   }
 
 }
